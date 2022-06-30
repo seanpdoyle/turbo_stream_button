aa61ff23ef021e087c56e67f6b41c9089fdcfde1
Sean Doyle <sean.p.doyle24@gmail.com>
2022-06-30 09:40:22 -0400

    [GENERATED]: `rails standard --fix`

--- test/dummy/config/initializers/replit.rb
+++ test/dummy/config/initializers/replit.rb
@@ -3,13 +3,13 @@ Rails.application.configure do
 
   if repl_slug.present? && repl_owner.present?
     config.action_controller.allow_forgery_protection = false
-    config.action_controller.default_url_options = { host: "#{repl_slug}.#{repl_owner}.repl.co" }
+    config.action_controller.default_url_options = {host: "#{repl_slug}.#{repl_owner}.repl.co"}
 
     config.session_store :cookie_store, same_site: :none, secure: true
 
     config.action_dispatch.default_headers = {
       "X-Frame-Options" => "ALLOWFROM replit.com",
-      "Access-Control-Allow-Origin" => "repl.co",
+      "Access-Control-Allow-Origin" => "repl.co"
     }
 
     config.hosts << /.*\.repl.co/
