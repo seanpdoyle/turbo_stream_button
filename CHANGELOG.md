e779e623cb5bc6eea840a8da3df55a2a1212a672
Sean Doyle <sean.p.doyle24@gmail.com>
2022-06-30 17:50:04 -0400

    Support merging multiple tokens in attributes
    Resolves issue where a token list containing multiple tokens was
    incorrectly invoking `Set#add`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- Support multiple tokens in token list mergers
 - Resolve `TurboStreamButton::Helpers` loading issue
 
 ## 0.2.0
--- lib/turbo_stream_button/html.rb
+++ lib/turbo_stream_button/html.rb
@@ -3,15 +3,15 @@ module TurboStreamButton
     def self.merge_token_lists(default, override)
       token_list = Set[*override.to_s.split]
 
-      token_list.add(*default.to_s.split)
+      default.to_s.split.each { |token| token_list.add(token) }
 
       token_list.to_a.join(" ")
     end
 
     def self.deep_merge_attributes(default, **attributes)
-      default.deep_merge(attributes) do |key, default, attribute|
+      default.deep_merge(attributes) do |key, default_value, attribute|
         if key.to_s.in? %w[controller action]
-          merge_token_lists(default, attribute)
+          merge_token_lists(default_value, attribute)
         else
           attribute
         end
